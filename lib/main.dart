05cc73856e5d08a07ab08c291b3118497750a180
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2023-03-06 09:40:55 +0300

    refactor(app): Clean code before PR

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:orioks/data/repository/token_repository.dart';
 
 // TODO: fetch schedule of group using id of student's group https://gitlab.com/orioks/student-api/-/blob/master/docs/schedule.rst
 // TODO: check if it is neccessary to update schedule https://gitlab.com/orioks/student-api/-/blob/master/docs/schedule.rst
@@ -41,27 +40,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _token = "no token yet";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(_token),
-            ElevatedButton(
-              onPressed: () async {
-                _token = (await TokenRepository().get()).token;
-                setState(() {});
-              },
-              child: const Text("Fetch token!"),
-            )
-          ],
-        ),
+      body: const Center(
+        child: Text("UI is in development"),
       ),
     );
   }
