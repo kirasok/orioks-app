7e995bdd8e995e83eba209f47ccb4ee2b3754c12
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2023-03-08 09:56:51 +0300

    feat(ui): implement bottom navigation bar
    Implement bottom navigation bar of 3 elements (schedule, subjects,
    students) using BLOC architecture

---
+++ lib/ui/screen/student_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StudentScreen extends StatefulWidget {
+  const StudentScreen({super.key});
+
+  @override
+  State<StudentScreen> createState() => _StudentScreenState();
+}
+
+class _StudentScreenState extends State<StudentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Student"));
+  }
+}
