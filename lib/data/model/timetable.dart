06675b3f1ea42e88eee229259f8b1cccc665b39e
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2023-03-09 11:33:28 +0300

    fix(model): schedule and timetable

--- lib/data/model/timetable.dart
+++ lib/data/model/timetable.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
 
+class Pair {
+  late TimeOfDay start;
+  late TimeOfDay end;
+  Pair(String start, String end) {
+    this.start = _parse(start);
+    this.end = _parse(end);
+  }
+
+  TimeOfDay _parse(String time) {
+    var parts = time.split(":");
+    return TimeOfDay(hour: int.parse(parts[0]), minute: int.parse(parts[1]));
+  }
+}
+
 class Timetable {
-  Map<int, List<TimeOfDay>> pairs;
+  Map<int, Pair> pairs;
 
   Timetable({
     required this.pairs,
   });
 
-  static fromJson(Map<String, dynamic> json) => json.map(
-        (key, value) => MapEntry(
-          int.parse(key),
-          [
-            TimeOfDay.fromDateTime(DateTime.parse(value[0])),
-            TimeOfDay.fromDateTime(DateTime.parse(value[1]))
-          ],
+  static fromJson(Map<String, dynamic> json) => Timetable(
+        pairs: json.map(
+          (key, value) {
+            var s = List<String>.from(value as List<dynamic>);
+            Pair pair = Pair(s[0], s[1]);
+            return MapEntry(int.parse(key), pair);
+          },
         ),
       );
 }
