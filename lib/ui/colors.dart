e6384839280a38608c789eb0391ea267ea009d2b
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2023-03-10 20:00:02 +0300

    feat(colors): add dark grade colors

--- lib/ui/colors.dart
+++ lib/ui/colors.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 List<num> _gradeZones = [0.0, 0.25, 0.5, 0.7, 0.85, 1.0];
 
-// TODO: add dark version
 var _gradeColors = [
   const Color(0x00ffffff),
   const Color(0xffe53935),
@@ -12,8 +11,22 @@ var _gradeColors = [
   const Color(0xff66bb6a),
 ];
 
-Color gradeToColor(num currentGrade, num maxGrade) => _gradeColors[
-    _gradeZones.indexWhere((element) => currentGrade <= maxGrade * element)];
+var _gradeColorsDark = [
+  const Color(0x00ffffff),
+  const Color(0xff93000d),
+  const Color(0xff693c00),
+  const Color(0xff574500),
+  const Color(0xff444b00),
+  const Color(0xff005318),
+];
+
+Color gradeToColor(num currentGrade, num maxGrade, BuildContext context) {
+  int index =
+      _gradeZones.indexWhere((element) => currentGrade <= maxGrade * element);
+  return Theme.of(context).brightness == Brightness.light
+      ? _gradeColors[index]
+      : _gradeColorsDark[index];
+}
 
 const seedLight = Color(0xff008cba);
 
