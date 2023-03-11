04be9927dae53e4e2c013c6ba6f3d2b19cc313dc
Kira Sokolova <Kyra256@proton.me>
2023-03-11 11:51:37 +0300

    feat(events): show discipline info in events screen

--- lib/ui/screen/events_screen.dart
+++ lib/ui/screen/events_screen.dart
@@ -1,27 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:orioks/data/model/discipline.dart';
 import 'package:orioks/logic/cubit/events_cubit.dart';
 import 'package:orioks/ui/widget/event_tile.dart';
 
 class EventsScreen extends StatelessWidget {
-  final int disciplineId;
-  final String disciplineName;
+  final Discipline discipline;
 
-  const EventsScreen(
-      {super.key, required this.disciplineId, required this.disciplineName});
+  const EventsScreen({super.key, required this.discipline});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(disciplineName),
+        title: Text(discipline.name),
       ),
       body: BlocBuilder<EventsCubit, EventsState>(builder: (context, state) {
         if (state is EventsLoaded) {
-          return ListView.builder(
-            itemBuilder: (context, index) =>
-                EventTile(event: state.events[index]),
-            itemCount: state.events.length,
+          List<Widget> teachers = [];
+          for (var value in discipline.teachers) {
+            teachers.add(
+              Text(
+                value,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            );
+          }
+          List<Widget> disciplineInformations = [
+                Text(
+                  discipline.departament,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                discipline.examDate != ""
+                    ? Text(discipline.examDate)
+                    : const SizedBox(),
+              ] +
+              teachers +
+              [const Divider()];
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: disciplineInformations +
+                [
+                  Expanded(
+                    child: ListView.builder(
+                      itemBuilder: (context, index) =>
+                          EventTile(event: state.events[index]),
+                      itemCount: state.events.length,
+                    ),
+                  ),
+                ],
           );
         } else if (state is EventsLoading) {
           return const Center(
