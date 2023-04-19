222ca22297cfc359d6f5d81ce48ef4f53d5b254f
Kira Sokolova <Kyra256@proton.me>
2023-04-19 16:36:49 +0300

    feat(demo): add demo mode

--- lib/data/repository/disciplines_repository.dart
+++ lib/data/repository/disciplines_repository.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 
+import 'package:orioks/constants.dart';
 import 'package:orioks/data/api/api_service.dart';
 import 'package:orioks/data/model/discipline.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DisciplinesRepository {
   static final DisciplinesRepository _instance = DisciplinesRepository._();
@@ -11,7 +14,14 @@ class DisciplinesRepository {
   factory DisciplinesRepository() => _instance;
 
   Future<List<Discipline>> get() async {
-    String json = await ApiService().fetchDisciplines();
+    String json;
+    if ((await SharedPreferences.getInstance())
+            .getBool(Constants.demoModeKey) ??
+        false) {
+      json = await rootBundle.loadString('templates/basic/disciplines.json');
+    } else {
+      json = await ApiService().fetchDisciplines();
+    }
     final List<dynamic> dynamicList = jsonDecode(json);
     final List<Discipline> disciplines =
         List<Discipline>.from(dynamicList.map((e) => Discipline.fromJson(e)));
