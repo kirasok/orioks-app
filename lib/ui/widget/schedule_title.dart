ee91bb9ae91affedbeff4c38f79275e1ffaf0a77
Kira Sokolova <Kyra256@proton.me>
2023-04-19 19:40:36 +0300

    feat(schedule): highlight current subject

--- lib/ui/widget/schedule_title.dart
+++ lib/ui/widget/schedule_title.dart
@@ -10,17 +10,36 @@ class ScheduleTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String type = "";
-    if (pair.type != "") {
-      type = "(${pair.type})";
+    String type = pair.type != "" ? "(${pair.type})" : "";
+    String start = "", end = "";
+    if (timetable.pairs[pair.pair] != null) {
+      final startTime = timetable.pairs[pair.pair]!.start;
+      final endTime = timetable.pairs[pair.pair]!.end;
+      start = startTime.format(context);
+      end = endTime.format(context);
+      final now = TimeOfDay.now();
+      if (startTime.hour <= now.hour &&
+          startTime.minute <= now.minute &&
+          now.hour <= endTime.hour &&
+          now.minute <= endTime.minute) {
+        return Card(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(24),
+            ),
+          ),
+          margin: const EdgeInsets.all(8),
+          child: _buildTile(type, start, end),
+        );
+      }
     }
-    String start = timetable.pairs[pair.pair]?.start.format(context) ?? "";
-    String end = timetable.pairs[pair.pair]?.end.format(context) ?? "";
-    return ListTile(
-      title: Text("${pair.name} $type"),
-      subtitle: Text(pair.teacher),
-      leading: Text("$start\n$end"),
-      trailing: Text(pair.location),
-    );
+    return _buildTile(type, start, end);
   }
+
+  Widget _buildTile(String type, String start, String end) => ListTile(
+        title: Text("${pair.name} $type"),
+        subtitle: Text(pair.teacher),
+        leading: Text("$start\n$end"),
+        trailing: Text(pair.location),
+      );
 }
